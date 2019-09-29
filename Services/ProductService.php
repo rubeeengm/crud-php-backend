5bf9259dc305b4eab7a1f92dd89557bf630fead2
rubeeengm <rubeeengm@outlook.com>
2019-09-29 16:19:51 -0500

    creacion de un producto

--- Services/ProductService.php
+++ Services/ProductService.php
@@ -4,6 +4,7 @@ namespace Services;
 
 use PDO;
 use PDOException;
+use Models\Product;
 use Database\DbProvider;
 
 class ProductService {
@@ -13,7 +14,7 @@ class ProductService {
         $this->_db = DbProvider::get();
     }
 
-    public function getAll() {
+    public function getAll() : Array {
         $result = [];
 
         try {
@@ -27,5 +28,43 @@ class ProductService {
         }
         
         return $result;
+    }
+
+    public function get(int $id) : Product {
+        $result = null;
+
+        try {
+            $stm = $this->_db->prepare('select * from products where id = :id');
+            
+            $stm->execute(['id' => $id]);
+            $data = $stm->fetchObject('\\Models\\Product');
+
+            if($data) {
+                 $result = $data;
+            }
+        } catch(PDOException $ex) {
+
+        }
+
+        return $result;
+    }
+
+    public function create(Product $model): void {
+        try {
+            $stm = $this->_db->prepare(
+                'insert into products(name, price, created_at, updated_at) values (:name, :price, :created, :updated)'
+            );
+
+            $now = date('Y-m-d H:i:s');
+
+            $stm->execute([
+                'name' => $model->name
+                , 'price' => $model->price
+                , 'created' => $now
+                , 'updated' => $now
+            ]);
+        } catch(PDOException $ex) {
+            var_dump($ex);
+        }
     }
 }
--- index.php
+++ index.php
@@ -3,9 +3,15 @@
 require_once 'vendor/autoload.php';
 require_once 'config.php';
 
+use Models\Product;
 use Services\ProductService;
 
 $productService = new ProductService();
-$result = $productService->getAll();
+
+$model = new Product;
+$model->name = 'Guitarra Suhr';
+$model->price = 3000;
+
+$result = $productService->create($model);
 
 var_dump($result);
